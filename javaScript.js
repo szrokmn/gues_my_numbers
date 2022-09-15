27b5add0ec610cbf654ea957aea4004a1a025637
Sezer Ökmen <szrokmn34@gmail.com>
2022-09-15 16:01:49 +0300

    js finished

--- javaScript.js
+++ javaScript.js
@@ -1,5 +1,5 @@
 // 1 - 100 arasında bir sayı tut
-const randomNumber = Math.round(Math.random() * 100);
+let randomNumber = Math.round(Math.random() * 100);
 console.log(randomNumber);
 
 // Variables
@@ -18,6 +18,7 @@ if(!guessInput) {
 } else if (randomNumber === guessInput){
     msg.innerHTML = `Congrats You Win <i class="fa-solid fa-face-grin-hearts fa-2x"></i>`;
     document.querySelector("body").style.background = "green";
+    document.querySelector(".check-btn").disabled = true;
 
     if (score > topScore) {
         topScore = score;
@@ -30,8 +31,8 @@ if(!guessInput) {
         score--;    // score u 1 azalt
 
     guessInput > randomNumber
-    ? (msg.innerHTML = `<i class="fa-solid fa-arrow-trend-up fa-2x"></i> INCREASE `)
-    : (msg.innerHTML= `<i class="fa-solid fa-arrow-trend-down fa-2x"></i> DECREASE `);
+    ? (msg.innerHTML = `<i class="fa-solid fa-arrow-trend-down fa-2x"></i> DECREASE`)
+    : (msg.innerHTML= `<i class="fa-solid fa-arrow-trend-up fa-2x"></i> INCREASE`);
     }else {
         msg.innerHTML = `You Lost <i class="fa-regular fa-face-sad-tear fa-2x"></i>`;
         document.querySelector("body").style.background = "red";
@@ -42,8 +43,24 @@ if(!guessInput) {
 }   
 });
 
-
-
+//* again basildiginda oyunu baslangic dgerlerin kur
+document.querySelector(".again-btn").addEventListener("click", () => {
+    score = 10;
+    document.querySelector(".score").textContent = score;
+    randomNumber = Math.round(Math.random() * 100);
+    document.querySelector(".secret-number").textContent = "?";
+    console.log(randomNumber);
+    document.querySelector(".check-btn").disabled = false;
+    document.querySelector("body").style.background = "#2d3436";
+    document.querySelector(".guess-input").value = "";
+    document.querySelector(".msg").innerText = `Starting..`;
+  });
+  
+  document.querySelector(".guess-input").addEventListener("keydown", (e) => {
+    if (e.code === "Enter") {
+      document.querySelector(".check-btn").click();
+    }
+  });
 
 
 
